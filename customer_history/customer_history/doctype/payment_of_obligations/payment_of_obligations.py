a2ec171b01ee98af7e8d44e9f54ad08ef948eb63
Hagar <hagermossad80@gmail.com>
2024-03-12 22:16:08 +0200

    calculate_residual

--- customer_history/customer_history/doctype/payment_of_obligations/payment_of_obligations.py
+++ customer_history/customer_history/doctype/payment_of_obligations/payment_of_obligations.py
@@ -1,8 +1,28 @@
-# Copyright (c) 2024, customer_history and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import now
 from frappe.model.document import Document
 
 class PaymentofObligations(Document):
-	pass
+	def before_save(self):
+		if self.commitment_number :
+			self.calculate_residual()
+
+	def before_submit(self):
+		if self.commitment_number :
+			self.append_payment_of_obligations()
+
+	def calculate_residual(self):
+		customer_history = frappe.get_doc("Customer History" , self.commitment_number)
+		self.residual = customer_history.payment_amount - customer_history.total_commitment_amount - self.payment_amount
+	
+	def append_payment_of_obligations(self):
+		customer_history = frappe.get_doc("Customer History" , self.commitment_number)
+		customer_history.append("commitment" , {
+				"payment_of_obligations" : self.name,
+				"commitment_amount" :self.payment_amount , 
+				"commitment_date" : now()
+		})
+		customer_history.total_commitment_amount += self.payment_amount
+		customer_history.save()
